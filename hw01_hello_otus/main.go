973de6ae9a8c1902471a5f05cb1518e89618c427
saphka <alex.loginov@pm.me>
2022-08-30 18:47:59 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/example/stringutil"
 )
 
 func main() {
 	const original = "Hello, OTUS!"
-	var reversed = stringutil.Reverse(original)
+	reversed := stringutil.Reverse(original)
 	fmt.Println(reversed)
 }
