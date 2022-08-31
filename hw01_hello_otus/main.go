c85e71382f0eb01224084337a21ff6b804f97ae9
Alexander Loginov <saphka@yandex.ru>
2022-08-31 19:35:56 +0300

    HW1 is completed (#1)
    * HW1 is completed
    
    * HW1 is completed
    
    * HW1 is completed
    
    * HW1 is completed
    
    * HW1 is completed
    
    Co-authored-by: saphka <alex.loginov@pm.me>

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	const original = "Hello, OTUS!"
+	fmt.Println(stringutil.Reverse(original))
 }
