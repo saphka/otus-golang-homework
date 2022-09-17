52748bc998037b9cef6984d8cc75cad96a08db0d
saphka <saphka@yandex.ru>
2022-09-17 10:37:21 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,54 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+)
+
+const maxResultSize = 10
+
+func Top10(input string) (result []string) {
+	result = make([]string, 0, maxResultSize)
+	if input == "" {
+		return
+	}
+
+	splitter := regexp.MustCompile("[\\n\\s\\t]+")
+	freqMap := make(map[string]int)
+	for _, s := range splitter.Split(input, -1) {
+		if s == "" {
+			continue
+		}
+		count := freqMap[s]
+		count++
+		freqMap[s] = count
+	}
+
+	type frequency struct {
+		str   string
+		count int
+	}
+	freqSlice := make([]frequency, 0, len(freqMap))
+	for k, v := range freqMap {
+		freqSlice = append(freqSlice, frequency{
+			str:   k,
+			count: v,
+		})
+	}
+	sort.Slice(freqSlice, func(i, j int) bool {
+		if freqSlice[i].count != freqSlice[j].count {
+			return freqSlice[i].count > freqSlice[j].count
+		} else {
+			return freqSlice[i].str < freqSlice[j].str
+		}
+	})
+	sliceLength := len(freqSlice)
+	if sliceLength > maxResultSize {
+		sliceLength = maxResultSize
+	}
+	for _, freq := range freqSlice[:sliceLength] {
+		result = append(result, freq.str)
+	}
+
+	return result
 }
