be58facf69cbc48334cb90167dc9feae90f67149
saphka <saphka@yandex.ru>
2022-10-29 15:58:15 +0500

    HW4 is completed

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -49,8 +49,66 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+	t.Run("purge logic simple", func(t *testing.T) {
+		c := NewCache(3)
+
+		wasInCache := c.Set("a", 10)
+		require.False(t, wasInCache)
+		wasInCache = c.Set("b", 20)
+		require.False(t, wasInCache)
+		wasInCache = c.Set("c", 30)
+		require.False(t, wasInCache)
+		wasInCache = c.Set("d", 40)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("a")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("b")
+		require.True(t, ok)
+		require.Equal(t, 20, val)
+
+		val, ok = c.Get("c")
+		require.True(t, ok)
+		require.Equal(t, 30, val)
+
+		val, ok = c.Get("d")
+		require.True(t, ok)
+		require.Equal(t, 40, val)
+	})
+
+	t.Run("purge logic read access", func(t *testing.T) {
+		c := NewCache(3)
+
+		wasInCache := c.Set("aa", 40)
+		require.False(t, wasInCache)
+		wasInCache = c.Set("bb", 50)
+		require.False(t, wasInCache)
+		wasInCache = c.Set("cc", 60)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("bb")
+		require.True(t, ok)
+		require.Equal(t, 50, val)
+
+		val, ok = c.Get("dd")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("aa")
+		require.True(t, ok)
+		require.Equal(t, 40, val)
+
+		wasInCache = c.Set("bb", 80)
+		require.True(t, wasInCache)
+
+		wasInCache = c.Set("dd", 70)
+		require.False(t, wasInCache)
+
+		val, ok = c.Get("cc")
+		require.False(t, ok)
+		require.Nil(t, val)
 	})
 }
 
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,79 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len        int
+	head, tail *ListItem
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newHead := &ListItem{
+		Value: v,
+	}
+	return l.pushFrontInternal(newHead)
+}
+
+func (l *list) pushFrontInternal(newHead *ListItem) *ListItem {
+	oldHead := l.head
+	l.head = newHead
+	l.head.Next = oldHead
+	l.head.Prev = nil
+	if oldHead != nil {
+		oldHead.Prev = l.head
+	} else {
+		l.tail = l.head
+	}
+	l.len++
+	return l.head
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	oldTail := l.tail
+	l.tail = &ListItem{
+		Value: v,
+		Prev:  oldTail,
+	}
+	if oldTail != nil {
+		oldTail.Next = l.tail
+	} else {
+		l.head = l.tail
+	}
+	l.len++
+	return l.tail
+}
+
+func (l *list) Remove(i *ListItem) {
+	switch {
+	case i.Prev != nil && i.Next != nil: // middle element
+		i.Next.Prev = i.Prev
+		i.Prev.Next = i.Next
+	case i.Prev != nil: // tail
+		l.tail = i.Prev
+		l.tail.Next = nil
+	case i.Next != nil: // head
+		l.head = i.Next
+		l.head.Prev = nil
+	default: // single element
+		l.head = nil
+		l.tail = nil
+	}
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	l.Remove(i)
+	l.pushFrontInternal(i)
 }
 
 func NewList() List {
