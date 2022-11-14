ca3e11c021c29b6864eb96056b83aaab3d5b7886
saphka <saphka@yandex.ru>
2022-11-14 22:59:44 +0500

    HW7 is complete

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,115 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeOffset        = errors.New("offset cannot be negative")
+	ErrNegativeLimit         = errors.New("limit cannot be negative")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+
+	var (
+		source, dest *os.File
+		err          error
+	)
+
+	source, err = os.Open(fromPath)
+	defer func(source io.Closer) {
+		_ = source.Close()
+	}(source)
+	if err != nil {
+		return fmt.Errorf("cannot open file %s: %w", fromPath, err)
+	}
+	var sizeLeft int64
+	if sizeLeft, err = seekOffset(source, offset); err != nil {
+		return fmt.Errorf("error during seek: %w", err)
+	}
+
+	dest, err = os.Create(toPath)
+	defer func(dest io.Closer) {
+		_ = dest.Close()
+	}(dest)
+	if err != nil {
+		return fmt.Errorf("cannot create file %s: %w", toPath, err)
+	}
+
+	if limit == 0 || limit > sizeLeft {
+		limit = sizeLeft
+	}
+	if err = copyContents(source, dest, limit); err != nil {
+		return fmt.Errorf("error in copy: %w", err)
+	}
+	return nil
+}
+
+func seekOffset(file *os.File, offset int64) (int64, error) {
+	stat, err := file.Stat()
+	if err != nil {
+		return 0, fmt.Errorf("cannot access file details %s: %w", file.Name(), err)
+	}
+	size := stat.Size()
+	if size < offset {
+		return 0, ErrOffsetExceedsFileSize
+	}
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return 0, fmt.Errorf("cannot seek to offset %d: %w", offset, err)
+	}
+	return size - offset, nil
+}
+
+func copyContents(source io.Reader, dest io.Writer, limit int64) error {
+	const bufferSize = 1024
+	buf := make([]byte, bufferSize)
+	var total int64
+	var prevProgress int
+
+	for {
+		shouldExit := false
+		bytesToWrite, err := source.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				shouldExit = true
+			} else {
+				return err
+			}
+		}
+
+		total += int64(bytesToWrite)
+		if total >= limit {
+			bytesToWrite -= int(total - limit)
+			shouldExit = true
+			total = limit
+		}
+
+		if bytesToWrite > 0 {
+			_, err = dest.Write(buf[:bytesToWrite])
+			if err != nil {
+				return err
+			}
+		}
+
+		progress := int(float32(total) / float32(limit) * 100)
+		if progress > prevProgress {
+			fmt.Printf("%d%%...", progress)
+			prevProgress = progress
+		}
+
+		if shouldExit {
+			break
+		}
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,77 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"math/rand"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("bad offset", func(t *testing.T) {
+		err := Copy("dummy", "dummy", -1, 0)
+		require.Equal(t, err, ErrNegativeOffset)
+	})
+	t.Run("bad limit", func(t *testing.T) {
+		err := Copy("dummy", "dummy", 0, -1)
+		require.Equal(t, err, ErrNegativeLimit)
+	})
+
+	t.Run("full copy", func(t *testing.T) {
+		largeBuf := make([]byte, 1024*1024)
+		f := generateFile(largeBuf)
+
+		dest := path.Join(os.TempDir(), "dest.txt")
+		err := Copy(f, dest, 0, 0)
+		require.Nil(t, err)
+
+		destF, err := os.Open(dest)
+		require.Nil(t, err)
+		contents, _ := io.ReadAll(destF)
+		_ = destF.Close()
+		require.Equal(t, largeBuf, contents)
+	})
+
+	t.Run("offset copy", func(t *testing.T) {
+		largeBuf := make([]byte, 1024*1024)
+		f := generateFile(largeBuf)
+
+		dest := path.Join(os.TempDir(), "dest.txt")
+		err := Copy(f, dest, 35, 0)
+		require.Nil(t, err)
+
+		destF, err := os.Open(dest)
+		require.Nil(t, err)
+		contents, _ := io.ReadAll(destF)
+		_ = destF.Close()
+		require.Equal(t, largeBuf[35:], contents)
+	})
+
+	t.Run("offset limit copy", func(t *testing.T) {
+		largeBuf := make([]byte, 1024*1024)
+		f := generateFile(largeBuf)
+
+		dest := path.Join(os.TempDir(), "dest.txt")
+		err := Copy(f, dest, 35, 42)
+		require.Nil(t, err)
+
+		destF, err := os.Open(dest)
+		require.Nil(t, err)
+		contents, _ := io.ReadAll(destF)
+		_ = destF.Close()
+		require.Equal(t, largeBuf[35:35+42], contents)
+	})
+}
+
+func generateFile(largeBuf []byte) string {
+	gen := rand.New(rand.NewSource(time.Now().Unix()))
+	gen.Read(largeBuf)
+	f, _ := os.CreateTemp("", "copy_test_*.txt")
+	_, _ = f.Write(largeBuf)
+	_ = f.Close()
+	return f.Name()
 }
