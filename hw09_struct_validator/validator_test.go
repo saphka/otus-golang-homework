f3b8827bc018172a442826ea1bf647c97ff8ed10
saphka <saphka@yandex.ru>
2022-11-21 21:42:58 +0500

    HW9 is complete

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -3,6 +3,7 @@ package hw09structvalidator
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -42,19 +43,39 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in:          App{Version: "1234"},
+			expectedErr: ValidationErrors{ValidationError{Field: "Version", Err: ErrLenConstraint}},
+		},
+		{
+			in:          App{Version: "12345"},
+			expectedErr: nil,
 		},
 		// ...
 		// Place your code here.
 	}
 
 	for i, tt := range tests {
+		tt := tt
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+			if tt.expectedErr != nil {
+				if valid, ok := tt.expectedErr.(ValidationErrors); ok {
+					var realErr ValidationErrors
+					require.ErrorAs(t, err, &realErr)
+					require.Equal(t, len(valid), len(realErr))
+					for i, expected := range valid {
+						require.Equalf(t, expected.Field, realErr[i].Field, "Field at pos %d must be equal", i)
+						require.ErrorAsf(t, realErr[i].Err, &expected.Err, "Error at pos %d must be of type %T", i, expected.Err)
+					}
+				} else {
+					require.ErrorAs(t, err, &tt.expectedErr)
+				}
+			} else {
+				require.NoError(t, err)
+			}
+
 		})
 	}
 }
