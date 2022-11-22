cc3c173a797c1489599e8bf224f76bbe86a8930c
saphka <saphka@yandex.ru>
2022-11-22 22:53:50 +0500

    HW9 is complete

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,9 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -35,6 +37,18 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	BadRegexp struct {
+		Dummy string `validate:"regexp:["`
+	}
+
+	IntLength struct {
+		Dummy int `validate:"len:34"`
+	}
+
+	StrangeStruct struct {
+		Dummy string `validate:"contains:some"`
+	}
 )
 
 func TestValidate(t *testing.T) {
@@ -50,6 +64,91 @@ func TestValidate(t *testing.T) {
 			in:          App{Version: "12345"},
 			expectedErr: nil,
 		},
+		{
+			in:          Response{Code: 502},
+			expectedErr: ValidationErrors{ValidationError{Field: "Code", Err: ErrInConstraint}},
+		},
+		{
+			in:          Response{Code: 500},
+			expectedErr: nil,
+		},
+		{
+			in:          Token{},
+			expectedErr: nil,
+		},
+		{
+			in: User{},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrLenConstraint},
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Email", Err: ErrRegexpConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+			},
+		},
+		{
+			in: User{Phones: []string{""}},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrLenConstraint},
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Email", Err: ErrRegexpConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+				ValidationError{Field: "Phones[0]", Err: ErrLenConstraint},
+			},
+		},
+		{
+			in: User{Phones: []string{"79054443377", "1234567"}},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrLenConstraint},
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Email", Err: ErrRegexpConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+				ValidationError{Field: "Phones[1]", Err: ErrLenConstraint},
+			},
+		},
+		{
+			in: User{ID: "308f06f2-5dc2-461e-a029-360ce6b4b24f", Email: "sample@test.com"},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+			},
+		},
+		{
+			in: User{
+				ID:    "308f06f2-5dc2-461e-a029-360ce6b4b24f",
+				Email: "sample@test.com",
+				Role:  "stuff",
+				meta:  json.RawMessage{},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+			},
+		},
+		{
+			in: User{ID: "308f06f2-5dc2-461e-a029-360ce6b4b24f", Email: "sample@test.com", Role: "stuff", Age: 123},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Age", Err: ErrMaxConstraint},
+			},
+		},
+		{
+			in:          User{ID: "308f06f2-5dc2-461e-a029-360ce6b4b24f", Email: "sample@test.com", Role: "stuff", Age: 34},
+			expectedErr: nil,
+		},
+		{
+			in:          BadRegexp{},
+			expectedErr: ErrBadValidateParam,
+		},
+		{
+			in:          "I am not a struct",
+			expectedErr: ErrNotStruct,
+		},
+		{
+			in:          IntLength{},
+			expectedErr: ValidationErrors{ValidationError{Field: "Dummy", Err: ErrWrongArgumentType}},
+		},
+		{
+			in:          StrangeStruct{Dummy: "fff"},
+			expectedErr: ErrUnknownValidateType,
+		},
 		// ...
 		// Place your code here.
 	}
@@ -60,22 +159,23 @@ func TestValidate(t *testing.T) {
 			t.Parallel()
 
 			err := Validate(tt.in)
+
 			if tt.expectedErr != nil {
-				if valid, ok := tt.expectedErr.(ValidationErrors); ok {
+				var valid ValidationErrors
+				if errors.As(tt.expectedErr, &valid) {
 					var realErr ValidationErrors
 					require.ErrorAs(t, err, &realErr)
 					require.Equal(t, len(valid), len(realErr))
 					for i, expected := range valid {
 						require.Equalf(t, expected.Field, realErr[i].Field, "Field at pos %d must be equal", i)
-						require.ErrorAsf(t, realErr[i].Err, &expected.Err, "Error at pos %d must be of type %T", i, expected.Err)
+						require.ErrorIsf(t, realErr[i].Err, expected.Err, "Error at pos %d must be of type %T", i, expected.Err)
 					}
 				} else {
-					require.ErrorAs(t, err, &tt.expectedErr)
+					require.ErrorIs(t, err, tt.expectedErr)
 				}
 			} else {
 				require.NoError(t, err)
 			}
-
 		})
 	}
 }
