ccc31937a41dd6f9137256ee0173420fe95d7c48
Alexander Loginov <saphka@yandex.ru>
2022-11-23 09:54:51 +0500

    HW9 is complete (#9)
    * HW9 is complete

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,14 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +17,216 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	sb := &strings.Builder{}
+	sb.WriteString(fmt.Sprintf("validation errors, total %d:", len(v)))
+
+	for _, err := range v {
+		sb.WriteString(fmt.Sprintf(" field %s error: %v;", err.Field, err.Err))
+	}
+
+	return sb.String()
 }
 
+var (
+	ErrNotStruct           = errors.New("value is not a structure")
+	ErrBadValidateParam    = errors.New("bad validate tag parameter")
+	ErrUnknownValidateType = errors.New("unknown validator type")
+	ErrWrongArgumentType   = errors.New("cannot validate value of this type")
+	ErrLenConstraint       = errors.New("string length must be equal to")
+	ErrRegexpConstraint    = errors.New("string must match regexp")
+	ErrInConstraint        = errors.New("value must be in")
+	ErrMinConstraint       = errors.New("value must be not be less than")
+	ErrMaxConstraint       = errors.New("value must be not be greater than")
+)
+
 func Validate(v interface{}) error {
-	// Place your code here.
+	valueOf := reflect.ValueOf(v)
+	if valueOf.Kind() != reflect.Struct {
+		return ErrNotStruct
+	}
+	var result ValidationErrors
+
+	for i := 0; i < valueOf.NumField(); i++ {
+		field := valueOf.Field(i)
+		fieldType := valueOf.Type().Field(i)
+		if !fieldType.IsExported() {
+			continue
+		}
+		valid, ok := fieldType.Tag.Lookup("validate")
+		if !ok {
+			continue
+		}
+
+		validator, err := getCompositeValidator(valid)
+		if err != nil {
+			return err
+		}
+
+		result = validateField(result, field, validator, fieldType.Name)
+	}
+
+	if len(result) > 0 {
+		return result
+	}
 	return nil
+}
+
+func validateField(
+	result ValidationErrors,
+	field reflect.Value,
+	validator compositeValidatorFunc,
+	name string,
+) ValidationErrors {
+	switch field.Kind() { //nolint:exhaustive
+	case reflect.Slice:
+		for i := 0; i < field.Len(); i++ {
+			result = append(result, validateField(nil, field.Index(i), validator, fmt.Sprintf("%s[%d]", name, i))...)
+		}
+	case reflect.Int, reflect.String:
+		err := validator(field)
+		if len(err) > 0 {
+			for _, singleErr := range err {
+				result = append(result, ValidationError{Field: name, Err: singleErr})
+			}
+		}
+	}
+	return result
+}
+
+type compositeValidatorFunc func(v reflect.Value) []error
+
+func getCompositeValidator(valid string) (compositeValidatorFunc, error) {
+	split := strings.Split(valid, "|")
+	validators := make([]validatorFunc, 0, len(split))
+	for _, singleValid := range split {
+		validator, err := getValidator(singleValid)
+		if err != nil {
+			return nil, err
+		}
+		validators = append(validators, validator)
+	}
+	return func(v reflect.Value) []error {
+		result := make([]error, 0, len(validators))
+		for _, validator := range validators {
+			err := validator(v)
+			if err != nil {
+				result = append(result, err)
+			}
+		}
+		if len(result) > 0 {
+			return result
+		}
+		return nil
+	}, nil
+}
+
+type validatorFunc func(v reflect.Value) error
+
+func getValidator(valid string) (validatorFunc, error) {
+	split := strings.SplitN(valid, ":", 2)
+	if len(split) < 2 {
+		return nil, ErrBadValidateParam
+	}
+
+	switch split[0] {
+	case "len":
+		return getLenValidator(split[1])
+	case "regexp":
+		return getRegexpValidator(split[1])
+	case "in":
+		return getInValidator(split[1])
+	case "min":
+		return getMinValidator(split[1])
+	case "max":
+		return getMaxValidator(split[1])
+	default:
+		return nil, ErrUnknownValidateType
+	}
+}
+
+func getLenValidator(param string) (validatorFunc, error) {
+	length, err := strconv.Atoi(param)
+	if err != nil {
+		return nil, ErrBadValidateParam
+	}
+	return func(v reflect.Value) error {
+		if v.Kind() != reflect.String {
+			return ErrWrongArgumentType
+		}
+		if len(v.String()) != length {
+			return fmt.Errorf("%w %d", ErrLenConstraint, length)
+		}
+		return nil
+	}, nil
+}
+
+func getRegexpValidator(param string) (validatorFunc, error) {
+	reg, err := regexp.Compile(param)
+	if err != nil {
+		return nil, ErrBadValidateParam
+	}
+	return func(v reflect.Value) error {
+		if v.Kind() != reflect.String {
+			return ErrWrongArgumentType
+		}
+		if !reg.MatchString(v.String()) {
+			return fmt.Errorf("%w %s", ErrRegexpConstraint, param)
+		}
+		return nil
+	}, nil
+}
+
+func getInValidator(param string) (validatorFunc, error) {
+	split := strings.Split(param, ",")
+	check := make(map[string]struct{}, len(split))
+	for _, val := range split {
+		check[val] = struct{}{}
+	}
+	return func(v reflect.Value) error {
+		var val string
+		switch v.Kind() { //nolint:exhaustive
+		case reflect.Int:
+			val = strconv.FormatInt(v.Int(), 10)
+		case reflect.String:
+			val = v.String()
+		default:
+			return ErrWrongArgumentType
+		}
+		if _, ok := check[val]; !ok {
+			return fmt.Errorf("%w (%s)", ErrInConstraint, param)
+		}
+		return nil
+	}, nil
+}
+
+func getMinValidator(param string) (validatorFunc, error) {
+	min, err := strconv.Atoi(param)
+	if err != nil {
+		return nil, ErrBadValidateParam
+	}
+	return func(v reflect.Value) error {
+		if v.Kind() != reflect.Int {
+			return ErrWrongArgumentType
+		}
+		if v.Int() < int64(min) {
+			return fmt.Errorf("%w %d", ErrMinConstraint, min)
+		}
+		return nil
+	}, nil
+}
+
+func getMaxValidator(param string) (validatorFunc, error) {
+	max, err := strconv.Atoi(param)
+	if err != nil {
+		return nil, ErrBadValidateParam
+	}
+	return func(v reflect.Value) error {
+		if v.Kind() != reflect.Int {
+			return ErrWrongArgumentType
+		}
+		if v.Int() > int64(max) {
+			return fmt.Errorf("%w %d", ErrMaxConstraint, max)
+		}
+		return nil
+	}, nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,8 +2,11 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -34,6 +37,18 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	BadRegexp struct {
+		Dummy string `validate:"regexp:["`
+	}
+
+	IntLength struct {
+		Dummy int `validate:"len:34"`
+	}
+
+	StrangeStruct struct {
+		Dummy string `validate:"contains:some"`
+	}
 )
 
 func TestValidate(t *testing.T) {
@@ -42,19 +57,125 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in:          App{Version: "1234"},
+			expectedErr: ValidationErrors{ValidationError{Field: "Version", Err: ErrLenConstraint}},
+		},
+		{
+			in:          App{Version: "12345"},
+			expectedErr: nil,
+		},
+		{
+			in:          Response{Code: 502},
+			expectedErr: ValidationErrors{ValidationError{Field: "Code", Err: ErrInConstraint}},
+		},
+		{
+			in:          Response{Code: 500},
+			expectedErr: nil,
+		},
+		{
+			in:          Token{},
+			expectedErr: nil,
+		},
+		{
+			in: User{},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrLenConstraint},
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Email", Err: ErrRegexpConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+			},
+		},
+		{
+			in: User{Phones: []string{""}},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrLenConstraint},
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Email", Err: ErrRegexpConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+				ValidationError{Field: "Phones[0]", Err: ErrLenConstraint},
+			},
+		},
+		{
+			in: User{Phones: []string{"79054443377", "1234567"}},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "ID", Err: ErrLenConstraint},
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Email", Err: ErrRegexpConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+				ValidationError{Field: "Phones[1]", Err: ErrLenConstraint},
+			},
+		},
+		{
+			in: User{ID: "308f06f2-5dc2-461e-a029-360ce6b4b24f", Email: "sample@test.com"},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+				ValidationError{Field: "Role", Err: ErrInConstraint},
+			},
+		},
+		{
+			in: User{
+				ID:    "308f06f2-5dc2-461e-a029-360ce6b4b24f",
+				Email: "sample@test.com",
+				Role:  "stuff",
+				meta:  json.RawMessage{},
+			},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Age", Err: ErrMinConstraint},
+			},
+		},
+		{
+			in: User{ID: "308f06f2-5dc2-461e-a029-360ce6b4b24f", Email: "sample@test.com", Role: "stuff", Age: 123},
+			expectedErr: ValidationErrors{
+				ValidationError{Field: "Age", Err: ErrMaxConstraint},
+			},
+		},
+		{
+			in:          User{ID: "308f06f2-5dc2-461e-a029-360ce6b4b24f", Email: "sample@test.com", Role: "stuff", Age: 34},
+			expectedErr: nil,
+		},
+		{
+			in:          BadRegexp{},
+			expectedErr: ErrBadValidateParam,
+		},
+		{
+			in:          "I am not a struct",
+			expectedErr: ErrNotStruct,
+		},
+		{
+			in:          IntLength{},
+			expectedErr: ValidationErrors{ValidationError{Field: "Dummy", Err: ErrWrongArgumentType}},
+		},
+		{
+			in:          StrangeStruct{Dummy: "fff"},
+			expectedErr: ErrUnknownValidateType,
 		},
 		// ...
 		// Place your code here.
 	}
 
 	for i, tt := range tests {
+		tt := tt
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+
+			if tt.expectedErr != nil {
+				var valid ValidationErrors
+				if errors.As(tt.expectedErr, &valid) {
+					var realErr ValidationErrors
+					require.ErrorAs(t, err, &realErr)
+					require.Equal(t, len(valid), len(realErr))
+					for i, expected := range valid {
+						require.Equalf(t, expected.Field, realErr[i].Field, "Field at pos %d must be equal", i)
+						require.ErrorIsf(t, realErr[i].Err, expected.Err, "Error at pos %d must be of type %T", i, expected.Err)
+					}
+				} else {
+					require.ErrorIs(t, err, tt.expectedErr)
+				}
+			} else {
+				require.NoError(t, err)
+			}
 		})
 	}
 }
